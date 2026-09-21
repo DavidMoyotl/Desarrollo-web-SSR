26bfc403f2984e0f90578c5aeabf027fb9b150af
DavidMoyotl <144041158+DavidMoyotl@users.noreply.github.com>
2026-09-21 19:58:13 +0000

    📶 Progress: ESM 2026 Refactor to ESM modules

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 // Funcion para manejar errores
-var createError = require('http-errors');
+import createError from 'http-errors'
 // Importa el framework express
-var express = require('express');
+// ❌ var express = require('express');
+import express from 'express'
 // Importa modulos para manejar rutas
-var path = require('path');
+// ❌ var path = require('path');
+import path from 'node:path'
 // Importa modulos para manejar cookies
-var cookieParser = require('cookie-parser');
+//  var cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser'
 // Importa modulos para manejar logs
-var logger = require('morgan');
+// ❌ var logger = require('morgan');
+import logger from 'morgan'
 
 // Importa rutas de la aplicacion
 var indexRouter = require('./routes/index');
